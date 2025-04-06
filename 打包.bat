63c559f2b24187c8323a50d47ae2edc920ea2f68
shuangmian-bai <2137089783@qq.com>
2025-04-06 10:06:01 +0800

    优化打包脚本

--- 打包.bat
+++ 打包.bat
@@ -1,4 +1,7 @@
 @echo off
+REM 设置变量是否为发布版本（1 为发布版本，0 为测试版本）
+set IS_RELEASE=0
+
 REM 检查 PyInstaller 是否已安装
 python -m pip install --upgrade pyinstaller
 
@@ -7,8 +10,14 @@ if exist "dist" (
     rmdir /s /q dist
 )
 
-REM 打包 Main.py 为多文件可执行文件，并设置图标，且不显示控制台窗口
-pyinstaller --noconsole --icon=./static/icon/shuangmian.ico Main.py
+REM 根据 IS_RELEASE 变量设置 pyinstaller 命令
+if %IS_RELEASE%==1 (
+    REM 打包 Main.py 为多文件可执行文件，并设置图标，且不显示控制台窗口
+    pyinstaller --noconsole --icon=./static/icon/shuangmian.ico Main.py
+) else (
+    REM 打包 Main.py 为多文件可执行文件，并设置图标，显示控制台窗口
+    pyinstaller --icon=./static/icon/shuangmian.ico Main.py
+)
 
 REM 复制 static 文件夹到 dist 目录
 xcopy /s /e /i "static" "dist\Main\static"

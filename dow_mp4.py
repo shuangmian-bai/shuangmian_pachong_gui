e6a482c7861f67d11e8b808bd0c7bde454c76dfc
shuangmian-bai <2137089783@qq.com>
2025-04-07 11:06:54 +0800

    去掉老项目的日志输出

--- dow_mp4.py
+++ dow_mp4.py
@@ -4,26 +4,8 @@ import threading
 import time
 import requests
 from requests.exceptions import RequestException, ConnectionError
-import logging
 from urllib3.exceptions import InsecureRequestWarning
 from PyQt6.QtCore import pyqtSignal, QObject
-
-# 手动创建日志文件并设置编码
-log_file = 'dow_mp4.log'
-with open(log_file, 'w', encoding='utf-8') as f:
-    pass
-
-# 配置日志记录器
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-# 创建文件处理器并设置编码
-file_handler = logging.FileHandler(log_file, encoding='utf-8')
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-
-# 将文件处理器添加到日志记录器
-logger.addHandler(file_handler)
 
 
 class DownloadProgress(QObject):
@@ -43,10 +25,8 @@ def retry_request(url, max_retries=3, backoff_factor=5):
         except (ConnectionError, RequestException) as e:
             retries += 1
             if retries < max_retries:
-                logging.error(f'请求失败: {url} (异常: {e}), 重试 {retries}/{max_retries}')
                 time.sleep(backoff_factor)
             else:
-                logging.error(f'请求失败: {url} (异常: {e}), 已达到最大重试次数')
                 raise
 
 
@@ -59,13 +39,11 @@ def download_ts(ts_url, file_path, semaphore, failed_urls, progress_signal=None,
                 raise Exception("Empty response")
             with open(file_path, 'wb') as f:
                 f.write(response.content)
-            logging.info(f'下载完成: {file_path}')
             if progress_signal:
                 completed_files += 1
                 progress_signal.emit(completed_files, total_files)
         except Exception as e:
             failed_urls.append(ts_url)
-            logging.error(f'下载失败: {ts_url} (异常: {e})')
             if progress_signal:
                 progress_signal.emit(completed_files, total_files)
 
@@ -119,8 +97,6 @@ def concatenate_ts_files(output_dir, output_file):
             with open(ts_file, 'rb') as infile:
                 outfile.write(infile.read())
 
-    logging.info(f'文件合并完成: {output_file}')
-
 
 def dow_mp4(ts_list, path, n, progress_signal=None):
     """主函数：下载并合并 TS 文件为 MP4"""
@@ -137,7 +113,6 @@ def dow_mp4(ts_list, path, n, progress_signal=None):
 
     # 检查输出文件是否已经存在
     if os.path.exists(output_file):
-        logging.info(f'输出文件已存在: {output_file}')
         if progress_signal:
             progress_signal.emit(len(ts_list), len(ts_list))  # 设置进度条为100%
         return
@@ -147,11 +122,8 @@ def dow_mp4(ts_list, path, n, progress_signal=None):
 
     # 检查下载结果
     if failed_urls:
-        logging.error('\n以下文件下载失败:')
-        for url in failed_urls:
-            logging.error(url)
+        pass
     else:
-        logging.info('\n所有文件下载成功')
         # 合成 mp4 文件
         concatenate_ts_files(output_dir, output_file)
         # 删除 ts 文件

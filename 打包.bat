f6efbfcf2614fa70eaaa1248413933ee5b3ed7e8
shuangmian-bai <2137089783@qq.com>
2025-04-28 15:33:34 +0800

    优化打包脚本，添加输出文件夹和文件名变量，简化命令设置

--- 打包.bat
+++ 打包.bat
@@ -3,6 +3,11 @@ chcp 65001 >nul
 REM 设置变量是否为发布版本（1 为发布版本，0 为测试版本）
 set IS_RELEASE=1
 
+REM 设置输出文件夹和文件名变量
+set OUTPUT_DIR=dist
+set RELEASE_NAME=shuangmian_release
+set DEBUG_NAME=shuangmian_debug
+
 REM 检查 PyInstaller 是否已安装
 python -m pyinstaller --version >nul 2>&1
 if %errorlevel% neq 0 (
@@ -10,18 +15,18 @@ if %errorlevel% neq 0 (
 )
 
 REM 删除 dist 文件夹（如果存在）
-if exist "dist" (
-    rmdir /s /q dist
+if exist "%OUTPUT_DIR%" (
+    rmdir /s /q "%OUTPUT_DIR%"
 )
 
 REM 设置 pyinstaller 命令的通用部分
-set PYINSTALLER_CMD=pyinstaller --icon=./static/icon/shuangmian.ico --name=shuangmian_pachong_gui --add-data "static;static" Main.py
+set PYINSTALLER_CMD=pyinstaller --icon=./static/icon/shuangmian.ico --add-data "static;static" Main.py
 
 REM 根据 IS_RELEASE 变量调整 pyinstaller 命令
 if %IS_RELEASE%==1 (
-    %PYINSTALLER_CMD% --noconsole --name=shuangmian_release
+    %PYINSTALLER_CMD% --noconsole --name=%RELEASE_NAME%
 ) else (
-    %PYINSTALLER_CMD% --name=shuangmian_debug
+    %PYINSTALLER_CMD% --name=%DEBUG_NAME%
 )
 
 REM 清理临时文件（可选）

f92f0524d6461556c93ee3aba157060602b49136
shuangmian-bai <2137089783@qq.com>
2025-04-28 15:52:20 +0800

    更新打包脚本，添加单文件可执行文件的生成逻辑，并优化发布和调试版本的打包命令

--- 打包.bat
+++ 打包.bat
@@ -7,6 +7,7 @@ REM 设置输出文件夹和文件名变量
 set OUTPUT_DIR=dist
 set RELEASE_NAME=pachong_gui
 set DEBUG_NAME=shuangmian_debug
+set SINGLE_EXE_NAME=pachong_gui_single
 
 REM 检查 PyInstaller 是否已安装
 python -m pyinstaller --version >nul 2>&1
@@ -24,12 +25,24 @@ set PYINSTALLER_CMD=pyinstaller --icon=./static/icon/shuangmian.ico --add-data "
 
 REM 根据 IS_RELEASE 变量调整 pyinstaller 命令
 if %IS_RELEASE%==1 (
+    REM 打包发布版本
     %PYINSTALLER_CMD% --noconsole --name=%RELEASE_NAME%
 ) else (
+    REM 打包调试版本
     %PYINSTALLER_CMD% --name=%DEBUG_NAME%
 )
 
-REM 清理临时文件（可选）
+REM 生成单文件的可执行文件，输出到当前目录
+if %IS_RELEASE%==1 (
+    REM 打包发布版本
+    %PYINSTALLER_CMD% --onefile --noconsole --name=%RELEASE_NAME% --distpath .
+) else (
+    REM 打包调试版本
+    %PYINSTALLER_CMD% --onefile --name=%DEBUG_NAME% --distpath .
+)
+
+
+REM 清理临时文件
 if exist "build" (
     rmdir /s /q build
 )

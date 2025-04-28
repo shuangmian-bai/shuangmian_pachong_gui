73c749a79a51e8c64968690f198f9ab48ff9ef39
shuangmian-bai <2137089783@qq.com>
2025-04-28 15:31:47 +0800

    优化打包脚本，添加 PyInstaller 版本检查，简化命令设置，并改进临时文件清理逻辑

--- 打包.bat
+++ 打包.bat
@@ -4,22 +4,30 @@ REM 设置变量是否为发布版本（1 为发布版本，0 为测试版本）
 set IS_RELEASE=1
 
 REM 检查 PyInstaller 是否已安装
-python -m pip install --upgrade pyinstaller
+python -m pyinstaller --version >nul 2>&1
+if %errorlevel% neq 0 (
+    python -m pip install --upgrade pyinstaller
+)
 
 REM 删除 dist 文件夹（如果存在）
 if exist "dist" (
     rmdir /s /q dist
 )
 
-REM 根据 IS_RELEASE 变量设置 pyinstaller 命令
+REM 设置 pyinstaller 命令的通用部分
+set PYINSTALLER_CMD=pyinstaller --icon=./static/icon/shuangmian.ico --name=shuangmian_pachong_gui --add-data "static;static" Main.py
+
+REM 根据 IS_RELEASE 变量调整 pyinstaller 命令
 if %IS_RELEASE%==1 (
-    REM 打包 Main.py 为多文件可执行文件，并设置图标，且不显示控制台窗口
-    pyinstaller --noconsole --icon=./static/icon/shuangmian.ico --name=shuangmian_pachong_gui --add-data "static;static" Main.py
+    %PYINSTALLER_CMD% --noconsole --name=shuangmian_release
 ) else (
-    REM 打包 Main.py 为多文件可执行文件，并设置图标，显示控制台窗口
-    pyinstaller --icon=./static/icon/shuangmian.ico --name=shuangmian_pachong_gui --add-data "static;static" Main.py
+    %PYINSTALLER_CMD% --name=shuangmian_debug
 )
 
 REM 清理临时文件（可选）
-rmdir /s /q build
-del /q *.spec
+if exist "build" (
+    rmdir /s /q build
+)
+if exist "*.spec" (
+    del /q *.spec
+)
